678fe6ba5e5520100ba646e9d82d69865c3249e2
Ahmad Fawwaz <92526178+fawwazjs@users.noreply.github.com>
2025-07-14 15:09:05 +0700

    Delete fesnuk-api directory

--- fesnuk-api/fesnuk-rest-api/README.md
+++
@@ -1,2 +0,0 @@
-# fesnuk-rest-api
-Penugasan Club Webdev x Hactiv8 Assignment Membangun REST API Social Media (User, Post, dan Like)
--- fesnuk-api/models/like.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Like struct {
-	ID     string `json:"id"`
-	UserID string `json:"user_id"`
-	PostID string `json:"post_id"`
-}
-
-var Likes []Like
